602fe24bad4d7172324f17d24147ebc58dcdd8b8
Hirak Saharia <64422300+hirak-saharia@users.noreply.github.com>
2025-10-16 22:06:45 +0530

    set up supabase

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button variant="destructive">Welcome to AI Finance</Button>;
+  return (
+    <div className="mt-40">
+      <Button variant="destructive">Welcome to AI Finance</Button>;
+    </div>
+  );
 }
--- components/header.js
+++ components/header.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { Button } from "./ui/button";
+import { PenBox, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+//import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+//import { checkUser } from "@/lib/checkUser";
+import Image from "next/image";
 
-function header() {
-  return <div>header</div>;
-}
+const Header = async () => {
+  //await checkUser();
 
-export default header;
+  return (
+    <header className="fixed top-0 w-full bg-blue-900 backdrop-blur-md z-50 border-b border-blue-900">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src={"/Saharia Zone.png"}
+            alt="Welth Logo"
+            width={200}
+            height={60}
+            className="h-14 w-auto object-contain"
+          />
+        </Link>
+
+        {/* Navigation Links - Different for signed in/out users */}
+
+        {/* Action Buttons */}
+        <div className="flex items-center space-x-4">
+          <Link
+            href="/dashboard"
+            className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+          >
+            <Button variant="outline">
+              <LayoutDashboard size={18} />
+              <span className="hidden md:inline">Dashboard</span>
+            </Button>
+          </Link>
+          <a href="/transaction/create">
+            <Button className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span className="hidden md:inline">Add Transaction</span>
+            </Button>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
